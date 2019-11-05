c00a4f07abe49ca3a684225aef5141d33622f1da
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-05 09:27:48 +0200

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+import random
+integer = random.randint (1,100)
+print(integer)
+float = random.uniform (1,50)
+print (float)
+answer1 = integer > float
+print("Is integer bigger than float?", answer1)
